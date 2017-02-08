feb2d4c741e678677d80184afecf4fe08ee3c035
RoryEgan <roryegan1997@gmail.com>
2017-02-08 11:48:26 +0000

    Section added to frontpage:
    Added a quick section to the frontpage and removed the inline styling from the cards. There should be no inline styling used as a general rule of thumb.

--- index.php
+++ index.php
@@ -1,34 +1,45 @@
 <?php include('includes/head.php');?>
 <?php include('includes/header.php');?>
 <div class="container">
-  <div class="row">
-    <div class="col-md-3">
-      <div class="card" style="width: 20rem;">
-        <div class="card-block">
-          <h4 class="card-title">Card 1</h4>
-          <p class="card-text">Some text here.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
+  <section>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+      ea commodo consequat. Duis aute irure dolor in reprehenderit in
+      voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
+      sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+      mollit anim id est laborum.</p>
+  </section>
+  <section>
+    <div class="row">
+      <div class="col-md-4">
+        <div class="card">
+          <div class="card-block">
+            <h4 class="card-title">Card 1</h4>
+            <p class="card-text">Some text here.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card">
+          <div class="card-block">
+            <h4 class="card-title">Card 2</h4>
+            <p class="card-text">Some text here.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="card">
+          <div class="card-block">
+            <h4 class="card-title">Card 3</h4>
+            <p class="card-text">Some text here.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
         </div>
       </div>
     </div>
-    <div class="col-md-3">
-      <div class="card" style="width: 20rem;">
-        <div class="card-block">
-          <h4 class="card-title">Card 2</h4>
-          <p class="card-text">Some text here.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-      </div>
-    </div>
-    <div class="col-md-3">
-      <div class="card" style="width: 20rem;">
-        <div class="card-block">
-          <h4 class="card-title">Card 3</h4>
-          <p class="card-text">Some text here.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-      </div>
-    </div>
-  </div>
+  </section>
 </div>
 <?php include('includes/footer.php');?>

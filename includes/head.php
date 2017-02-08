274ff160ca92cfe569fae101882a11355569862f
RoryEgan <roryegan1997@gmail.com>
2017-02-08 11:59:50 +0000

    Bootstrap 4 set up locally:
    I have set up a bower.json file to handle the installation of bootstrap and JQuery. Running bower install should install all the dependencies needed. I have also set up a gitignore file for the bower components as these will be local files and there's no point pushing and pulling them.

--- includes/head.php
+++ includes/head.php
@@ -2,6 +2,6 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
   <title>CS 4014 Project</title>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
 </head>

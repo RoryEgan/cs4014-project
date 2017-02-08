e3014dc344053747c7735c3cba0bcc323bd578de
RoryEgan <roryegan1997@gmail.com>
2017-02-08 10:53:40 +0000

    Php includes added:
    I have split up the header, footer and head into their own files inside an includes folder. They can now be included on each page via php as I have done in the index. This allows for better code reusability as each page will have a common header footer and head.

---
+++ includes/footer.php
@@ -0,0 +1,18 @@
+<footer>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4">
+        Footer Text 1
+      </div>
+      <div class="col-md-4">
+        Footer Text 2
+      </div>
+      <div class="col-md-4">
+        Footer Text 3
+      </div>
+      <div class="col-md-12">
+        <p>Website by Group 2</p>
+      </div>
+    </div>
+  </div>
+</footer>
---
+++ includes/header.php
@@ -0,0 +1,12 @@
+<header>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8">
+        Header Text 1
+      </div>
+      <div class="col-md-4">
+        Header Text 2
+      </div>
+    </div>
+  </div>
+</header>

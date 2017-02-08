7dd8abedf3b6da1ac4b1393db6e2623e122794f3
RoryEgan <roryegan1997@gmail.com>
2017-02-08 10:18:15 +0000

    First commit:
    This is a first commit with a very basic unstyled index page. Bootstrap 4 is being imported in the head via the Bootstrap CDN. This needs to be changed so we are using a local copy of Bootstrap. This commit is really just to get an initial file in the repo that we can begin developing.

---
+++ index.php
@@ -0,0 +1,72 @@
+<html>
+  <head>
+     <meta charset="utf-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1">
+     <title>CS 4014 Project</title>
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+  </head>
+  <header>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8">
+          Header Text 1
+        </div>
+        <div class="col-md-4">
+          Header Text 2
+        </div>
+      </div>
+    </div>
+  </header>
+  <body>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-3">
+          <div class="card" style="width: 20rem;">
+            <div class="card-block">
+              <h4 class="card-title">Card 1</h4>
+              <p class="card-text">Some text here.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="card" style="width: 20rem;">
+            <div class="card-block">
+              <h4 class="card-title">Card 2</h4>
+              <p class="card-text">Some text here.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="card" style="width: 20rem;">
+            <div class="card-block">
+              <h4 class="card-title">Card 3</h4>
+              <p class="card-text">Some text here.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4">
+          Footer Text 1
+        </div>
+        <div class="col-md-4">
+          Footer Text 2
+        </div>
+        <div class="col-md-4">
+          Footer Text 3
+        </div>
+        <div class="col-md-12">
+          <p>Website by Group 2</p>
+        </div>
+      </div>
+    </div>
+  </footer>
+</html>

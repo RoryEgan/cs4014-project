07e5e3ec71fb2c86871ddf14a556aaddbac5ad45
RoryEgan <roryegan1997@gmail.com>
2017-02-09 17:10:12 +0000

    Basic navbar implemented:
    I have set up a sticky navbar where half the navbar scrolls away and the other half becomes fixed. The navbar needs a bit more work, specifically a search bar.

--- assets/js/custom-jquery.js
+++ assets/js/custom-jquery.js
@@ -20,4 +20,50 @@ $(document).ready(function() {
 
 	})
 
+	$(window).load(function() {
+
+		// executes when complete page is fully loaded, including all frames, objects and images
+
+		// Sticky Nav
+		// ----------------------------------------------------------------------------
+		var $sticky_navigation = $('#sticky-nav-wrap');
+
+		if( $sticky_navigation.length ) {
+
+			// grab the initial top offset of the navigation
+			var sticky_navigation_offset_top = $sticky_navigation.offset().top;
+
+			// our function that decides whether the navigation bar should have "fixed" css position or not.
+			var sticky_navigation = function(){
+
+				var scroll_top = $(window).scrollTop(); // our current vertical position from the top
+
+				if (scroll_top > sticky_navigation_offset_top + 50 ) {
+
+					$('#sticky-nav-wrap .navbar').removeClass('navbar-static-top').addClass('fixed-top');
+					$( ".wrap" ).css( "margin-top", "50px" );
+
+				} else {
+
+					$('#sticky-nav-wrap .navbar').removeClass('fixed-top').addClass('navbar-static-top');
+					$( ".wrap" ).css( "margin-top", "0" );
+
+				}
+
+			};
+
+			// Run the function on load
+			sticky_navigation();
+
+			// ...and run it again every time the user scrolls
+			$(window).scroll(function() {
+
+				sticky_navigation();
+
+			});
+
+		}
+
+	});
+
 });

17f9b219aefcacbb92d90bf233281f6631ee5a58
RoryEgan <roryegan1997@gmail.com>
2017-02-10 13:13:01 +0000

    Search bar added to the navbar:
    A basic search bar has been added. It is still unfinished, as it needs to be optimised for mobile view. There is also an issue with selecting the colour change when you hover on the search icon.

--- includes/header.php
+++ includes/header.php
@@ -15,16 +15,28 @@
       <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-        <div class="nav-menu">
-          <ul class="navbar-nav mr-auto mt-2 mt-md-0">
-            <li class="active nav-item"><a href="#">Page 1</a></li>
-            <li class="active nav-item"><a href="#">Page 2</a></li>
-            <li class="active nav-item"><a href="#">Page 3</a></li>
-            <li class="active nav-item"><a href="#">Page 4</a></li>
-            <li class="active nav-item"><a href="#">Page 5</a></li>
-          </ul>
+      <div class="col-md-8">
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+          <div class="nav-menu">
+            <ul class="navbar-nav mr-auto mt-2 mt-md-0">
+              <li class="active nav-item"><a href="#">Page 1</a></li>
+              <li class="active nav-item"><a href="#">Page 2</a></li>
+              <li class="active nav-item"><a href="#">Page 3</a></li>
+              <li class="active nav-item"><a href="#">Page 4</a></li>
+              <li class="active nav-item"><a href="#">Page 5</a></li>
+            </ul>
+          </div>
         </div>
+      </div>
+      <div class="col-md-4">
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="text" placeholder="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
+            <i class="fa fa-search">
+              <a href="#"></a>
+            </i>
+          </button>
+        </form>
       </div>
     </div>
   </header>

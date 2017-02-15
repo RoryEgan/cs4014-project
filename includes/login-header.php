bed6b21ab6663982cfb53942b05f2f37c397feac
RoryEgan <roryegan1997@gmail.com>
2017-02-15 14:45:36 +0000

    Fixed up the login page:
    I have made a new include for the login-header. I have also styled up the whole page, aligning both the sign in and sign up forms to the right and moving the main heading to the left in the header. All filler text has been removed.

---
+++ includes/login-header.php
@@ -0,0 +1,22 @@
+<header class="banner hidden-sm-down">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 text-left">
+        <a href="/cs4014">
+          <h1 class="header-heading">Our Website</h1>
+        </a>
+      </div>
+      <div class="login-form col-md-6">
+        <form class="form-inline" action="" method="">
+          <div class="form-group">
+            <label class="mr-2" for="email">Email: </label>
+            <input id="email" class="form-control" type="email" placeholder="email"/>
+            <label class="mx-2" for="password">Password: </label>
+            <input id="password" type="password" class="form-control" placeholder="password"/>
+            <input type="submit" class="btn btn-default" value="login">
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</header>

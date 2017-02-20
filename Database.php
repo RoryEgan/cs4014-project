e0e54ac92a8b88b511d68f5d3707ab00b92f1749
JamieMac96 <jamiemac96@hotmail.com>
2017-02-20 13:30:21 +0000

    Database class created and login security risks decreased.
    I created a Database class (Database.php) that will handle queries,
     connections with the database and escaping user input to be sent to the database in query strings.
    I also changed the database connection method so that the connection information (eg database name, username, password)
    is stored outside of the site root directory in a .ini configuration file.

---
+++ Database.php
@@ -0,0 +1,60 @@
+<?php
+class Database{
+  protected static $connection;
+
+  function connect(){
+    // Try and connect to the database
+     if(!isset(self::$connection)) {
+         // Load configuration as an array. Use the actual location of your configuration file
+         $config = parse_ini_file('/var/www/html/config.ini');
+         self::$connection = new mysqli('localhost',$config['username'],$config['password'],$config['dbname']);
+     }
+
+     // If connection was not successful, handle the error
+     if(self::$connection === false) {
+          echo "<script>alert('connection failed')</script>";
+         return false;
+     }
+
+     return self::$connection;
+  }
+
+  //this function performs select queries to the database.
+  public function select($query) {
+    $rows = array();
+
+    $result = $this -> query($query);
+
+    //check if query returns no result / false.
+    if($result === false) {
+        return false;
+    }
+
+    //iterate through select query results and create corresponding array.
+    while ($row = $result -> fetch_assoc()) {
+        $rows[] = $row;
+    }
+
+    return $rows;
+  }
+
+  //This function removes dangerous characters from user input.
+  function quote($value){
+    //ensure we are connected to database by calling this class's function connect().
+    $connection = $this -> connect();
+
+    //escape characters problematic characters passed and return result.
+    return mysqli_real_escape_string($connection,$value);
+  }
+
+  //This function allows us to query the database.
+  function query($query){
+    //ensure we are connected to database by calling this class's function connect().
+    $connection = $this -> connect();
+
+    //call the query function on the connection object.
+    $result  = $connection -> query($query);
+
+    return $result;
+  }
+}

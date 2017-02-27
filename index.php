c63204a2ddc501173eaa59b5c49bd7eff171dbc5
RoryEgan <roryegan1997@gmail.com>
2017-02-27 21:40:44 +0000

    New task modal optimised:
    The new task modal now has a styled button that is ready to be placed wherever it needs to go. The form has been abstracted into a new includes folder called partial. The new task page has been deleted as it was unnecessary.

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
 <?php include('includes/head.php');?>
 <?php include('includes/header.php');?>
-<?php include('includes/php/utils/Database.class.php'); ?>
 <div class="page-content my-5">
   <div class="container">
 
     <section>
-      <a href="#" data-toggle="modal" data-target="#contact-modal-2" class="btn btn-default btn-md ghost modal-trigger" data-id="">
-        Select
-      </a>
+      <button type="button" class="btn btn-outline-primary">
+        <a href="#" data-toggle="modal" data-target="#contact-modal-2" class="modal-trigger">
+          New Task
+        </a>
+      </button>
       <div class="modal" id="contact-modal-2" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
         <div class="modal-dialog" role="document">
           <div class="modal-content">
@@ -15,57 +16,10 @@
               <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
               </button>
-              <h4 class="modal-title" id="myModalLabel">Get Customers Online</h4>
+              <h4 class="modal-title" id="myModalLabel">Create a new Task</h4>
             </div>
             <div class="modal-body">
-              <form class="form" onsubmit="checkHoneypot()" action="login.php" method="post">
-                <div class="form-group">
-                  <div>
-                    <input  class="form-control my-2" type="text" name="signUpFirstName" value="" placeholder="Task Title" required/>
-                  </div>
-                  <select class="form-control my-2" name="signUpSubject">
-                    <option selected hidden>Task Type</option>
-                    <?php
-                    include('includes/php/utils/DropdownOptionGenerator.class.php');
-                    $gen = new DropdownOptionGenerator();
-                    $query = "SELECT * FROM TaskType;";
-                    $gen -> generateOptions($query, 'TaskTypeVal');
-                    ?>
-                  </select>
-                  <div>
-                    <textarea name="signUpLastName" class="form-control my-2 input-large" type="text" value="" placeholder="Description"  required></textarea>
-                  </div>
-                  <div class="form-inline">
-                    <input name="signUpEmail" class="form-control my-2" type="text" placeholder="Number of pages" required/>
-                  </div>
-                  <div class="form-inline">
-                    <input name="signUpPassword" type="password" class="form-control my-2" placeholder="Number of words" />
-                  </div>
-                  <select class="form-control my-2" name="signUpSubject">
-                    <option selected hidden>File Format</option>
-                    <?php
-                    $query = "SELECT * FROM Format;";
-                    $gen -> generateOptions($query, 'FormatVal');
-                    ?>
-                  </select>
-                  <div class="my-2">
-                    <input type="file" class="my-2 btn-default btn-file" required/>
-                  </div>
-                  <label for="claim-date">Claim deadline:</label>
-                  <div class=" my-2">
-                    <input id="claim-date" type="date" />
-                  </div>
-                  <label for="completion-date">Completion deadline:</label>
-                  <div class=" my-2">
-                    <input id="completion-date" type="date" />
-                  </div>
-                  <input type="submit" class="btn btn-default" value="Submit" name="signUpButton" role="button"/>
-                  <div class="input-field">
-                    <input id="gotcha" type="text" name="contact" value="" />
-                  </div>
-                </div>
-              </form>
-
+              <?php include('includes/partial/new-task.php');?>
             </div>
           </div>
         </div>

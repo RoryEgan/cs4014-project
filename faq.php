788f1c6f6a80ef35a6476084b1dd0c726f07deb2
RoryEgan <roryegan1997@gmail.com>
2017-02-27 19:00:50 +0000

    More faqs added:
    I have added some more faq cards. The text for the faqs needs a colour change and the initial position of the chevrons need to be rotated.

--- faq.php
+++ faq.php
@@ -6,18 +6,98 @@
 
     <div id="accordion" class="accordion" role="tablist" aria-multiselectable="true">
 
-      <div class="card">
-        <div class="card-header" role="tab" id="heading">
+      <div class="card my-2">
+        <div class="card-header" role="tab" id="heading1">
           <h5 class="mb-0">
-            <a data-toggle="collapse" data-parent="#accordion" href="#collapse" aria-expanded="true" aria-controls="collapse">
+            <a data-toggle="collapse" data-parent="#accordion" href="#collapse1" aria-expanded="true" aria-controls="collapse">
               Question 1
               <i class="togglestate fa fa-chevron-down"></i>
             </a>
           </h5>
         </div>
-        <div id="collapse" class="collapse" role="tabpanel" aria-labelledby="heading">
+        <div id="collapse1" class="collapse" role="tabpanel" aria-labelledby="heading">
           <div class="card-block">
             Answer 1
+          </div>
+        </div>
+      </div>
+
+      <div class="card my-2">
+        <div class="card-header" role="tab" id="heading2">
+          <h5 class="mb-0">
+            <a data-toggle="collapse" data-parent="#accordion" href="#collapse2" aria-expanded="true" aria-controls="collapse">
+              Question 2
+              <i class="togglestate fa fa-chevron-down"></i>
+            </a>
+          </h5>
+        </div>
+        <div id="collapse2" class="collapse" role="tabpanel" aria-labelledby="heading">
+          <div class="card-block">
+            Answer 2
+          </div>
+        </div>
+      </div>
+
+      <div class="card my-2">
+        <div class="card-header" role="tab" id="heading3">
+          <h5 class="mb-0">
+            <a data-toggle="collapse" data-parent="#accordion" href="#collapse3" aria-expanded="true" aria-controls="collapse">
+              Question 3
+              <i class="togglestate fa fa-chevron-down"></i>
+            </a>
+          </h5>
+        </div>
+        <div id="collapse3" class="collapse" role="tabpanel" aria-labelledby="heading">
+          <div class="card-block">
+            Answer 3
+          </div>
+        </div>
+      </div>
+
+      <div class="card my-2">
+        <div class="card-header" role="tab" id="heading4">
+          <h5 class="mb-0">
+            <a data-toggle="collapse" data-parent="#accordion" href="#collapse4" aria-expanded="true" aria-controls="collapse">
+              Question 4
+              <i class="togglestate fa fa-chevron-down"></i>
+            </a>
+          </h5>
+        </div>
+        <div id="collapse4" class="collapse" role="tabpanel" aria-labelledby="heading">
+          <div class="card-block">
+            Answer 4
+          </div>
+        </div>
+      </div>
+
+      <div class="card my-2">
+        <div class="card-header" role="tab" id="heading5">
+          <h5 class="mb-0">
+            <a data-toggle="collapse" data-parent="#accordion" href="#collapse5" aria-expanded="true" aria-controls="collapse">
+              Question 5
+              <i class="togglestate fa fa-chevron-down"></i>
+            </a>
+          </h5>
+        </div>
+        <div id="collapse5" class="collapse" role="tabpanel" aria-labelledby="heading">
+          <div class="card-block">
+            Answer 5
+          </div>
+        </div>
+      </div>
+
+      <div class="card my-2">
+        <div class="card-header" role="tab" id="heading6">
+          <h5 class="mb-0">
+            <a data-toggle="collapse" data-parent="#accordion" href="#collapse6" aria-expanded="true" aria-controls="collapse">
+              Question 6
+              <i class="togglestate fa fa-chevron-down"></i>
+            </a>
+          </h5>
+        </div>
+        <div id="collapse6" class="collapse" role="tabpanel" aria-labelledby="heading">
+          <div class="card-block">
+            Answer 6
           </div>
         </div>
       </div>

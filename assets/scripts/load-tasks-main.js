c4f37cd1371fa4dfc009197f38fbd8a641bb3960
JamieMac96 <jamiemac96@hotmail.com>
2017-03-27 13:25:05 +0100

    Cron job functionality added, fixed modals in firefox, fixed and issue with the more button on the home page

--- assets/scripts/load-tasks-main.js
+++ assets/scripts/load-tasks-main.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
   var count = 1;
-  if($("#stop-loading").length){
+
+  if($("#stop-loading-main").length){
     $("#remove_row").remove();
   }
 
   $('#btn_more').on('click', function(){
+    alert("click");
     $.post('includes/php/scripts/display-tasks-main.php',{'count': count} ,function(data){
       $("#display-tasks").append(data);
       if($("#stop-loading-main").length){
--- includes/php/scripts/dynamic-modal.php
+++ includes/php/scripts/dynamic-modal.php
@@ -2,11 +2,9 @@
 //This is a script that can be included anywhere and it will add a modal to the page.
 //All that needs to be done is define a modalTitle and the URL of the modal we are going to include in order for this to work.
 ?>
-<button type="button" class="btn btn-outline" role="button">
-  <a href="#" data-toggle="modal" data-target="<?php echo '#' . $target; ?>" class="modal-trigger">
+  <a class="btn btn-outline" href="#" data-toggle="modal" data-target="<?php echo '#' . $target; ?>" class="modal-trigger">
     <?php echo $modalTitle; ?>
   </a>
-</button>
 <div class="modal" id="<?php echo $target; ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">

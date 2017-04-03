186f41a35d617c356f91f097f89fde5c945146ba
RoryEgan <roryegan1997@gmail.com>
2017-04-03 17:45:13 +0100

    Dynamic dropdown should now be working as intended. Needs testing in mobile views.

--- includes/partial/dynamic-search-task.php
+++ includes/partial/dynamic-search-task.php
@@ -1,3 +1,3 @@
-<a class="dropdown-item" href="task-details.php?taskID=<?php echo "$taskID"?>">
+<a class="dropdown-item" href="task-details.php?taskID=<?php echo $taskID ?>">
   <?php echo "$title"; ?>
 </a>
